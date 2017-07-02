f72a690c12fb244613a3a933f685e47210752084
Thomas Theunen <thomas.theunen@fenego.be>
2017-07-02 17:42:05 +0200

    Fix proxy with parameter

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 // Load the http module to create an http server.
 var http = require('http');
 var request = require('request');
+
 var port = process.env.PORT || 8080;
 
 // Configure our HTTP server to respond with Hello World to all requests.
@@ -10,11 +11,15 @@ var server = http.createServer(function(req, resp) {
     resp.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
     resp.setHeader('Access-Control-Allow-Credentials', true);
 
-    if (req.url === '/server.php') {
+    var query = require('url').parse(req.url, true).query;
+
+    console.log('Request url: ' + req.url);
+
+    if (req.url.indexOf('server.php') > 0) {
         var x;
-        if (req.query && req.query.host) {
-            console.log('Sending request to: ' + 'https://' + host + '/Serv.php')
-            x = request('https://' + host + '/Serv.php');
+        if (query && query.host) {
+            console.log('Sending request to: https://' + query.host + '/Serv.php')
+            x = request('https://' + query.host + '/Serv.php');
         } else {
             console.log('Sending request to: https://global.quickconnect.to/Serv.php')
             x = request('https://global.quickconnect.to/Serv.php')
