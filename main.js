aae5c2aca32b54d1a56e95ec74b2b9ad2cd434eb
Thomas Theunen <thomas.theunen@fenego.be>
2017-07-02 13:58:33 +0200

    Change port to heroku port

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 // Load the http module to create an http server.
 var http = require('http');
 var request = require('request');
-
+var port = process.env.PORT || 8080;
 
 // Configure our HTTP server to respond with Hello World to all requests.
 var server = http.createServer(function(req, resp) {
@@ -13,7 +13,7 @@ var server = http.createServer(function(req, resp) {
 });
 
 // Listen on port 8000, IP defaults to 127.0.0.1
-server.listen(8000);
+server.listen(port);
 
 // Put a friendly message on the terminal
-console.log("Server running at http://127.0.0.1:8000/");
+console.log("Server running at http://127.0.0.1:" + port);
